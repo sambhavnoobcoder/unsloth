bf12f4702ea15978e4b957d747ffca631e21aaea
sambhavnoobcoder <indosambhav@gmail.com>
2025-03-12 14:16:54 +0530

    create test image function

--- unsloth/tests/test_image_resize.py
+++ unsloth/tests/test_image_resize.py
@@ -5,3 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gc
 
+# Create a synthetic test image
+def create_test_image(size=(800, 600)):
+    # Create a colorful test image
+    img = np.zeros((size[1], size[0], 3), dtype=np.uint8)
+    # Add some colors and patterns
+    for i in range(size[1]):
+        for j in range(size[0]):
+            img[i, j, 0] = (i * 256) // size[1]  # R channel
+            img[i, j, 1] = (j * 256) // size[0]  # G channel
+            img[i, j, 2] = ((i+j) * 128) // (size[0]+size[1])  # B channel
+    return Image.fromarray(img)
