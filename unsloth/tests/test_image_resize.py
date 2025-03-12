0722209c8a3855ab13c439cb6a0f545b9ad8cd7c
sambhavnoobcoder <indosambhav@gmail.com>
2025-03-12 14:17:43 +0530

    create and display images

--- unsloth/tests/test_image_resize.py
+++ unsloth/tests/test_image_resize.py
@@ -16,3 +16,13 @@ def create_test_image(size=(800, 600)):
             img[i, j, 1] = (j * 256) // size[0]  # G channel
             img[i, j, 2] = ((i+j) * 128) // (size[0]+size[1])  # B channel
     return Image.fromarray(img)
+
+# Create test image
+image = create_test_image(size=(800, 600))
+print(f"Original image size: {image.size}")
+
+# Display the image
+plt.figure(figsize=(10, 6))
+plt.imshow(image)
+plt.title(f"Test image: {image.size[0]}x{image.size[1]}")
+plt.show()
